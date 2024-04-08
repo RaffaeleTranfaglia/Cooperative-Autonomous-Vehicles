45c95f3ae3aecee60a02926401edad6ec45d99ac
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-04-08 17:08:37 +0200

    add create_platoon method

--- Platoon.py
+++ Platoon.py
@@ -1,8 +1,9 @@
 import traci
+import random
 from plexe import Plexe, ACC, CACC
 
 # inter-vehicle distance
-DISTANCE = 5
+DISTANCE = 1.5
 
 class PlatoonManager:
     plexe = Plexe()
@@ -17,10 +18,16 @@ class PlatoonManager:
             vid = sorted_vids[i]
             traci.vehicle.setSpeedMode(vid, 0)
             if vid == sorted_vids[0]:
+                traci.vehicle.setColor(vid, [random.randint(0, 254) for x in range(3)].append(1))
                 cls.plexe.set_active_controller(vid, ACC)
                 leader = vid
             else:
+                traci.vehicle.setColor(vid, traci.vehicle.getColor(leader))
                 cls.plexe.set_active_controller(vid, CACC)
-                cls.plexe.set_path_cacc_parameters(vid, distance=traci.vehicle.getLength(vid)*2)
+                cls.plexe.set_path_cacc_parameters(vid, distance=DISTANCE)
                 topology[vid] = {"front" : sorted_vids[i-1], "leader" : leader}
+        return topology
+    
+# create a platoon when the next traffic light (the one after the tl in the current edge) 
+# is the same for each vehicle using traci.vehicle.getNextTLS()
             
