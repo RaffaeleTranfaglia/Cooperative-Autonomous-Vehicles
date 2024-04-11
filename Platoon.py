f7ee1ffa70b0b0aedec7034de15a961bd19a8649
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-04-11 19:14:17 +0200

    platooning v0.1

--- Platoon.py
+++ Platoon.py
@@ -1,14 +1,11 @@
 import traci
 import random
-from plexe import Plexe, ACC, CACC
+from plexe import ACC, CACC
 
 # inter-vehicle distance
 DISTANCE = 1
 
 class PlatoonManager:
-    def __init__(self, plexe = Plexe()):
-        self.plexe = plexe
-    
     """ 
     create a platoon of n vehicles
     :param lid: leader id
@@ -17,21 +14,42 @@ class PlatoonManager:
     indicates, for each vehicle, who is its leader and who is its front
     vehicle.
     """
-    def create_platoon(self, lid, vids):
+    @staticmethod
+    def create_platoon(lid, vids, plexe):
         traci.vehicle.setSpeedMode(lid, 0)
-        traci.vehicle.setColor(vid, [random.randint(0, 254) for x in range(3)].append(1))
-        self.plexe.set_active_controller(vid, ACC)
+        traci.vehicle.setColor(lid, [random.randint(0, 254) for x in range(3)].append(1))
+        plexe.set_active_controller(lid, ACC)
         topology = {}
         for i in range(len(vids)):
             vid = vids[i]
+            frontvid = vids[i-1] if i-1>=0 else lid
             traci.vehicle.setSpeedMode(vid, 0)
             traci.vehicle.setColor(vid, traci.vehicle.getColor(lid))
-            self.plexe.set_active_controller(vid, CACC)
-            self.plexe.set_path_cacc_parameters(vid, distance=DISTANCE)
-            topology[vid] = {"front" : vids[i], "leader" : lid}
-            #self.plexe.add_member(lid, vid, i)
+            plexe.set_active_controller(vid, CACC)
+            plexe.set_path_cacc_parameters(vid, distance=DISTANCE)
+            topology[vid] = {"front" : frontvid, "leader" : lid}
+            plexe.enable_auto_feed(vid, True, lid, frontvid)
         return topology
     
-# create a platoon when the next traffic light (the one after the tl in the current edge) 
-# is the same for each vehicle using traci.vehicle.getNextTLS()
-            
+    @staticmethod
+    def add_vehicle(topology, vid, frontvid, plexe):
+        if not topology: return
+        lid = topology[frontvid]["leader"]
+        traci.vehicle.setSpeedMode(vid, 0)
+        traci.vehicle.setColor(vid, traci.vehicle.getColor(lid))
+        plexe.set_activate_controller(vid, CACC)
+        plexe.set_path_cacc_parameters(vid, distance=DISTANCE)
+        topology[vid] = {"front" : frontvid, "leader" : lid}
+        plexe.enable_auto_feed(vid, True, lid, frontvid)
+        return
+        
+    @staticmethod
+    def free_platoon(lid, topology, plexe):
+        if not topology: return
+        traci.vehicle.setSpeedMode(lid, 31)
+        traci.vehicle.setColor(lid, [255,255,255,1])
+        for vid in topology:
+            traci.vehicle.setSpeedMode(vid, 31)
+            traci.vehicle.setColor(vid, [255,255,255,1])
+            plexe.set_active_controller(ACC)
+            plexe.enable_auto_feed(vid, False)
--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import traci
+from plexe import Plexe
+from Platoon import PlatoonManager
 
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
@@ -10,10 +12,21 @@ else:
     
 STEPS = 500
 
-def isPlatoonMember(vid, platoons):
-    for t in platoons[traci.vehicle.getLaneID()]:
-        if vid in t : return True
-    return False
+def getPlatoonTopology(vid, platoons):
+    for topologies in platoons[traci.vehicle.getLaneID(vid)]:
+        for t in topologies:
+            if vid in t : return t
+    return None
+
+def getNextEdge(vid):
+    edges = traci.vehicle.getRoute(vid)
+    curr = traci.vehicle.getRoadID(vid)
+    i = 0
+    while i <= len(edges):
+        if edges[i] == curr:
+            return edges[i]
+    return None
+    
 
 if __name__ == "__main__":
     sumoCmd = ["sumo-gui", "--step-length", "0.1",
@@ -22,23 +35,53 @@ if __name__ == "__main__":
     traci.start(sumoCmd)
     
     platoons = {}
+    last_members = []
     all_junctions = traci.trafficlight.getIDList()
+    
+    plexe = Plexe()
+    traci.addStepListener(plexe)
     
     step = 0
     while step < STEPS:
         traci.simulationStep()
         
+        # check on last platoons members here
+        for v, e in last_members[:]:    # Iterate over a copy of the list
+            if traci.vehicle.getRoadID(v) == e:
+                topology = getPlatoonTopology(v, platoons)
+                lid = topology[v]["leader"] if topology else None
+                PlatoonManager.free_platoon(lid, topology, plexe)
+                last_members.remove((v, e))
+        
         for junction in all_junctions:
             controlled_lanes = traci.trafficlight.getControlledLanes(junction)
             for lane in controlled_lanes:
+                platoons[lane] = []
                 vids = traci.lane.getLastStepVehicleIDs(lane)
-                
                 i = len(vids) - 1
                 while i >= 0:
-                    vid = vids[i]
-                    if not isPlatoonMember(vid, platoons):
-                        # continue from here
-                        pass
+                    ti = getPlatoonTopology(vids[i], platoons)
+                    if not ti:
+                        if (i+1 < len(vids) and getNextEdge(vids[i]) == getNextEdge(vids[i+1])):
+                            tnext = getPlatoonTopology(vids[i+1], platoons)
+                            PlatoonManager.add_vehicle(tnext if tnext else None, vids[i], vids[i+1], plexe)
+                            for v, e in last_members:
+                                if v == vids[i+1]:
+                                    last_members.remove((v, e))
+                                    last_members.append((vids[i], getNextEdge(vids[i])))
+                        
+                        lid = vids[i]
+                        j = i - 1
+                        next_edg = getNextEdge(lid)
+                        print(f"vehicle: {lid} -  route {next_edg}")
+                        members = []
+                        while j >= 0 and next_edg == getNextEdge(vids[j]):
+                            members.append(vids[j])
+                            j -= 1
+                        platoons[lane].append(PlatoonManager.create_platoon(lid, members, plexe))
+                        last_members.append((vids[j+1], getNextEdge(vids[j+1])))
+                        i = j
+                        
                 
                 
     traci.close()
