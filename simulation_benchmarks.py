21567271055f4b27b21e2ebe17163b974dfcbbda
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-10-23 19:42:36 +0200

    add metrics computation

--- simulation_benchmarks.py
+++ simulation_benchmarks.py
@@ -1,4 +1,53 @@
+import bisect
+import sys
 import xml.etree.ElementTree as ET
+
+def get_metrics(benchmark):
+    trips = len(benchmark)
+    total_waiting_time = 0
+    min_time = sys.float_info.max
+    max_time = sys.float_info.min
+    sorted_waiting_times = []
+    for info in benchmark:
+        waiting_time = float(info.get("waitingTime"))/60
+        total_waiting_time += waiting_time
+        max_time = max(max_time, waiting_time)
+        min_time = min(min_time, waiting_time)
+        bisect.insort(sorted_waiting_times, waiting_time)
+    
+    # Given that trips is the throughput per hour, compute the throughput per minute
+    throughput = trips / 60
+    
+    # Average waiting time per vehicle
+    avg_waiting_time = total_waiting_time/trips
+    
+    # Throughput to waiting time ratio, combines the two metrics to give a single measure of system efficency
+    # Higher values indicate a better-performing system where throughput is maximized while waiting time is minimized.
+    tw_ratio = throughput / avg_waiting_time
+    
+    # Traffic Efficiency Index, the overall efficiency of traffic flow, 
+    # taking into account how smoothly vehicles move through the system.
+    # Since the throughput unity of time is 1 hour, total waiting time is transformed in hours.
+    tei = throughput / total_waiting_time
+    
+    # using the percentile compute the pX of the average
+    # in other words, get the percentage of vehicles that are waiting less or equal than the average
+    # in addition, the same thing using a neighbourhood of the average -> average +- epsilon
+    epsilon = avg_waiting_time * 30 / 100
+    index = bisect.bisect_right(sorted_waiting_times, avg_waiting_time + epsilon)
+    percentage = index * 100 / len(sorted_waiting_times)
+    
+    return {
+        "throughput_per_hour": trips,
+        "throughput_per_min": round(throughput, 2),
+        "total_waiting_time": round(total_waiting_time, 2),
+        "avg_waiting_time": round(avg_waiting_time, 2),
+        "min_time": round(min_time, 2),
+        "max_time": round(max_time, 2),
+        "tw_ratio": round(tw_ratio, 2),
+        "tei": round(tei, 5),
+        "percentage": round(percentage, 2)
+    }
 
 if __name__ == "__main__":
     feed1 = "sim_cfg_grid_3_lanes/tripinfo.xml"
@@ -14,67 +63,37 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Error: {e}")
         
-    trips_1 = len(tripinfos1)
-    trips_2 = len(tripinfos2)
+    platooning_metrics = get_metrics(tripinfos1)
+    normal_metrics = get_metrics(tripinfos2)
         
-    print(f"Total trips ended using platooning strategy: {trips_1}")
-    total_waiting_time_1 = 0
-    for tripinfo1 in tripinfos1:
-        total_waiting_time_1 += float(tripinfo1.get("waitingTime"))
-        #print(tripinfo1.get("waitingTime"))
+    print(f"Total trips ended using platooning strategy: {platooning_metrics['throughput_per_hour']}")
+    print(f"Total trips ended: {normal_metrics['throughput_per_hour']}")
         
-    print(f"Total trips ended: {trips_2}")
-    total_waiting_time_2 = 0
-    for tripinfo2 in tripinfos2:
-        total_waiting_time_2 += float(tripinfo2.get("waitingTime"))
-        #print(tripinfo2.get("waitingTime"))
-        
-    trips_increment = trips_1 - trips_2
-    percent_trips_increment = trips_increment * 100 / trips_2
+    trips_increment = platooning_metrics["throughput_per_hour"] - normal_metrics["throughput_per_hour"]
+    percent_trips_increment = trips_increment * 100 / normal_metrics["throughput_per_hour"]
     print(f"Trips increment by using platooning strategy: {round(percent_trips_increment, 2)}%\n")
     
-    print(f"Total waiting time using platooning strategy: {round(total_waiting_time_1, 2)}")
-    print(f"Total waiting time: {round(total_waiting_time_2, 2)}")
+    print(f"Throughput per minute using platooning strategy: {platooning_metrics['throughput_per_min']}")
+    print(f"Throughput per minute: {normal_metrics['throughput_per_min']}\n")
     
-    waiting_time_reduction = total_waiting_time_2 - total_waiting_time_1
-    percent_waiting_time_reduction = waiting_time_reduction * 100 / total_waiting_time_2
-    print(f"Reduction of total waiting time by using platooning strategy: {round(percent_waiting_time_reduction, 2)}%")
+    print(f"Total waiting time using platooning strategy: {platooning_metrics['total_waiting_time']}")
+    print(f"Total waiting time: {normal_metrics['total_waiting_time']}")
     
-    #import numpy as np
-    #loss = 0
-    '''
-    b=[]
-    for child in root1:
-        a = float(child.attrib['timeLoss'])
-        #list = [a]
-        b.append(a)
-        #print(list,' ')
-    print(b)
+    waiting_time_reduction = normal_metrics['total_waiting_time'] - platooning_metrics['total_waiting_time']
+    percent_waiting_time_reduction = waiting_time_reduction * 100 / normal_metrics["total_waiting_time"]
+    print(f"Reduction of total waiting time by using platooning strategy: {round(percent_waiting_time_reduction, 2)}%\n")
     
-    #Caculate average losstime
-    loss = 0
-    for child in root1:
-        a = float(child.attrib['timeLoss'])
-        loss += a
-    ave_loss = loss / len(root1)
-    print('ave_loss =', ave_loss)
-    print('%.2f'%ave_loss)
-    #Caculate average variance
-    difference = 0
-    for child in root1:
-        a = float(child.attrib['timeLoss'])
-        difference += (a - ave_loss)**2
-    sigma = difference / len(root1)
-    print('sigma =', sigma)
-    print('%.2f'%sigma)
-    print('The number of the vehicles is', len(root1))
-
-    #Caculate average speed
-    sum_v = 0
-    for child in root1:
-        v = float(child.attrib['arrivalSpeed'])
-        sum_v += v
-    ave_v = sum_v / len(root1)
-    print('ave_v is', ave_v)
-    print('%.2f'%ave_v)
-    '''
+    print(f"Average waiting time per vehicle using platooning strategy: {platooning_metrics['avg_waiting_time']}")
+    print(f"Average waiting time per vehicle: {normal_metrics['avg_waiting_time']}\n")
+    
+    print(f"Min and Max waiting time using platooning strategy: ({platooning_metrics['min_time']},{platooning_metrics['max_time']})")
+    print(f"Min and Max waiting time: ({normal_metrics['min_time']},{normal_metrics['max_time']})\n")
+    
+    print(f"Throughput to waiting time ratio using platooning strategy: {platooning_metrics['tw_ratio']}")
+    print(f"Throughput to waiting time ratio: {normal_metrics['tw_ratio']}\n")
+    
+    print(f"Traffic efficiency index using platooning strategy: {platooning_metrics['tei']}")
+    print(f"Traffic efficiency index: {normal_metrics['tei']}\n")
+    
+    print(f"Percentage of vehicle whose waiting time meets the average using platooning strategy: {platooning_metrics['percentage']}%")
+    print(f"Percentage of vehicle whose waiting time meets the average: {normal_metrics['percentage']}%")
